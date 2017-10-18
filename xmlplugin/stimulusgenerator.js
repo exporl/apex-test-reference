ed738c67b9cab6361a8cb26c1e61d1e180f7250d
sanne raymaekers <sanne.raymaekers@gmail.com>
2017-10-18 14:15:46 +0200

    Use pluginscriptlibrary in xmlplugins
    Pluginscriptlibrary is updated across schema versions.

--- xmlplugin/stimulusgenerator.js
+++ xmlplugin/stimulusgenerator.js
@@ -1,12 +1,4 @@
 function getStimuli () {
-
-return    "<stimulus id=\"stimulus1\">" +
-      "<datablocks>" +
-        "<datablock id=\"rawdata1\"/>" +
-      "</datablocks>" +
-      "<variableParameters/>" +
-      "<fixedParameters/>" +
-    "</stimulus>" ;
-
+    return xml.stimulus("stimulus1", "rawdata1");
 }
 
